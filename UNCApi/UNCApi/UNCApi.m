33e6f0b974501d5eeeb05fb2afa458e71ad13f58
unchi <developer.japan@gmail.com>
2016-08-12 21:39:12 +0900

    コメント削除

--- UNCApi/UNCApi/UNCApi.m
+++ UNCApi/UNCApi/UNCApi.m
@@ -110,85 +110,12 @@ static const NSString* const LINE_END = @"\r\n";
     }];
 }
 
-//- (void)_requestBody:(NSString*)url
-//             method:(NSString*)method
-//             params:(NSDictionary*)params
-//            headers:(NSDictionary*)headers
-//  completionHandler:(UNCApiHandler)func {
-//    
-//    NSMutableDictionary* strParams = [NSMutableDictionary new];
-//    NSMutableDictionary* binParams = [NSMutableDictionary new];
-//    
-//    for (NSString* key in params) {
-//
-//        id o = [params objectForKey: key];
-//        
-//        if ([o isKindOfClass:[UNCApiData class]]) {
-//            [binParams setObject:o forKey: key];
-//        } else {
-//            [strParams setObject:o forKey: key];
-//        }
-//    }
-//    
-//    AFHTTPSessionManager *manager = [AFHTTPSessionManager new];
-//    AFHTTPRequestSerializer* serializer = [AFHTTPRequestSerializer serializer];
-//
-//    for (NSString* key in headers) {
-//        NSString* val = [headers objectForKey: key];
-//        [serializer setValue: val forHTTPHeaderField: key];
-//    }
-//    serializer.timeoutInterval = _timeoutInterval;
-//
-//    NSMutableURLRequest* request = nil;
-//    
-//    if ([binParams count] > 0) {
-//        request = [serializer multipartFormRequestWithMethod:method
-//                                                   URLString:url
-//                                                  parameters:strParams
-//                                   constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-//
-//                        // XXX データは ネストに対応していない
-//                                       
-//                        for (NSString* key in binParams) {
-//                            UNCApiData* data = [binParams objectForKey: key];
-//                            [formData appendPartWithFileData:data.bin
-//                                                        name:key
-//                                                    fileName:data.fileName
-//                                                    mimeType:data.mimeType];
-//                        }
-//
-//                     }
-//                                                       error:nil];
-//    } else {
-//        
-//        request = [serializer requestWithMethod:method URLString:url parameters:strParams error:nil];
-//    }
-//
-//    manager.requestSerializer = serializer;
-//    
-//    __block NSURLSessionDataTask *task =
-//    [manager uploadTaskWithStreamedRequest:request
-//                                  progress:nil
-//                         completionHandler:^(NSURLResponse * __unused response, id responseObject, NSError *error) {
-//
-//        if (error) {
-//            func(nil, (NSHTTPURLResponse*)[task response], error);
-//        } else {
-//            func(responseObject, (NSHTTPURLResponse*)[task response], nil);
-//        }
-//    }];
-//    
-//    [task resume];
-//}
-
 - (void)_requestSerializeBody:(NSString*)urlStr
                        method:(NSString*)method
                        params:(NSDictionary*)params
                       headers:(NSDictionary*)headers
             completionHandler:(UNCApiHandler)func {
     
-    
-    // AFNetworking使ってない
     NSURL* const url = [NSURL URLWithString:urlStr];
     
     NSString* boundary = [UNCApi _boundaryGenarate];
